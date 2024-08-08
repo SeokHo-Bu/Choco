544cf369c543020bc2fc1c9f3d15bcfd97649b9b
ralph <choco@osiris.kr>
2024-08-08 15:19:47 +0900

    MUI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
+// src/App.js
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.222333
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Stack direction="row" spacing={2}>
+            <Button variant="outlined">Primary</Button>
+            <Button variant="outlined" disabled>
+                Disabled
+            </Button>
+            <Button variant="outlined" href="#outlined-buttons">
+                Link
+            </Button>
+        </Stack>
+    );
 }
 
 export default App;
