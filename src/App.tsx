d0cf45ec709ea337e99f2a46b8d3bd54e00628c8
ralph <choco@osiris.kr>
2024-08-22 10:11:25 +0900

    버튼 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from './stories/Button'
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [primarys, setPrimarys] = useState(false);
+
+    const onClickEvent = () => {
+        setCount(count + 1);
+        setPrimarys(!primarys);
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.222333
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button label={`12345 => ${count}`} onClick={() => onClickEvent()} primary={primarys}/>
     </div>
   );
 }
